d701274b233d55b56f7007ffd1fea8513b4e519e
Bishal Gurung <grgbishal360@gmail.com>
2026-02-12 13:50:20 +0545

    fix: fixed an issue with max_attempts and decay_seconds being received as string from config

--- config/notification.php
+++ config/notification.php
@@ -3,7 +3,7 @@
 return [
 
     'rate_limit' => [
-        'max_attempts' => env("NOTIFICATION_RATE_LIMIT_MAX_ATTEMPTS", 10),
-        'decay_seconds' => env("NOTIFICATION_RATE_LIMIT_DECAY_SECONDS", 3600),
+        'max_attempts' => (int) env("NOTIFICATION_RATE_LIMIT_MAX_ATTEMPTS", 10),
+        'decay_seconds' => (int) env("NOTIFICATION_RATE_LIMIT_DECAY_SECONDS", 3600),
     ],
 ];
